d0ac8621e2d692377ebd70cf3387abca74537cd7
Aoi <ht.so.ik0102@gmail.com>
2021-06-07 10:01:09 +0700

    modified index.js

--- blockchain-app/quiz_module/index.js
+++ blockchain-app/quiz_module/index.js
@@ -1,10 +1,24 @@
 const { BaseModule } = require("lisk-sdk");
 const { QuestionAccountSchema } = require("./schemas");
+const { getAllQuestionsAsJSON } = require("./question");
+
+const CreateQueestionAsset = require("./transactions/create_question_asset");
+const AnswerQuestionAsset = require("./transactions/answer_question_asset");
 
 class QuizModule extends BaseModule {
     name = "question";
     id = 1024;
     accountSchema = QuestionAccountSchema;
+
+    transactionAssets = [
+        new CreateQueestionAsset(),
+        new AnswerQuestionAsset(),
+    ];
+
+    actions = {
+        getAllQuestions: async () => getAllQuestionsAsJSON(this._dataAccess),
+    };
+        
 }
 
 module.exports = QuizModule;
