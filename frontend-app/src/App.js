aa9e0484917ce9353ab97dd07ee33525116c754f
Aoi <ht.so.ik0102@gmail.com>
2021-06-09 23:32:01 +0700

    update landing page

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+import React, { Fragment, useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Link as RouterLink,
+  Switch,
+  Route
+} from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Link,
+  IconButton,
+  Container,
+  Chip,
+  Icon
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import { nodeInfoContextDefaultValue, NodeInfoContext } from "./context";
+
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  appBarLink: {
+    margin: theme.spacing(0, 2), 
+    flex: 1,
+  },
+  speedDial: {
+    position: 'absolute', 
+    bottom: theme.spacing(2), 
+    right: theme.spacing(2),
+  },
+  contentContainer: {
+    padding: theme.spacing(5, 6),
+  }
+}));
+
 function App() {
+  const classes = useStyles();
+  const [nodeInfoState, updateNodeInfoState] = useState(nodeInfoContextDefaultValue);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <NodeInfoContext.Provider value={nodeInfoState}>
+        <Router>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6">QUIZ APP</Typography>
+
+              <Link
+                color="inherit"
+                component={RouterLink}
+                to="/"
+                className={classes.appBarLink}
+              >
+                Home
+              </Link>
+              <Link
+                color='inherit'
+                component={RouterLink}
+                to="/transactions"
+                className={classes.appBarLink}
+              >
+                Transactions
+              </Link>
+              <div className={classes.grow} />
+              <Chip label={nodeInfoState.height} />
+            </Toolbar>
+          </AppBar>
+
+          
+        </Router>
+
+      </NodeInfoContext.Provider>
+      <div className="App">
+
+      </div>
+    </Fragment>
+
   );
 }
 
