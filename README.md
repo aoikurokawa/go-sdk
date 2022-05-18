c74c2affea8e33e0972ae22927e3699182b45906
Aoi <ht.so.ik0102@gmail.com>
2022-05-18 23:14:49 +0700

    set up

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# QuizAppLisk
+# AMM
--- frontend-app/src/api/index.js
+++
@@ -1,5 +0,0 @@
-export const fetchNodeInfo = async () => {
-    return fetch("http://localhost:4000/api/node/info")
-    .then((res) => res.json())
-    .then((res) => res.data);
-};
